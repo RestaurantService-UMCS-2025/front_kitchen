01b63e1635ef46dfa2a2ed7e049b5c6ed402fd72
Tomki2258 <tomek.rybinski@interia.pl>
2026-03-25 13:13:12 +0100

    Can toggle available menu positions

--- kitchen-nightmare/src/assets/ProductList.jsx
+++ kitchen-nightmare/src/assets/ProductList.jsx
@@ -1,18 +1,39 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 
 function ProductList() {
-    const [products, setProducts] = useState([
-        { id: 1, name: 'Dwa Bułki', checked: false },
-        { id: 2, name: 'Mięso', checked: false },
-        { id: 3, name: 'Sałata', checked: false },
-        { id: 4, name: 'Japko', checked: false },
-        { id: 5, name: 'Pomidor', checked: false },
-    ]);
+    const [products, setProducts] = useState([]);
+    //const [products, setProducts] = useState([
+    //     { id: 1, name: 'Dwa Bułki', checked: false },
+    //     { id: 2, name: 'Mięso', checked: false },
+    //     { id: 3, name: 'Sałata', checked: false },
+    //     { id: 4, name: 'Japko', checked: false },
+    //     { id: 5, name: 'Pomidor', checked: false },
+    // ]);
+
+    useEffect(() => {
+        fetch('http://localhost:5077/api/Menu/all')
+            .then(response => response.json())
+            .then(json => {
+                setProducts(json)
+            })
+            .catch(error => console.error(error))
+    }, []);
 
     const toggleProduct = (id) => {
-        setProducts(prev =>
-            prev.map(p => p.id === id ? { ...p, checked: !p.checked } : p)
-        );
+        fetch(`http://localhost:5077/api/Menu/available`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: id,
+                mode: false
+            })
+        }).then(() => {
+            setProducts(prev =>
+                prev.map(p => p.id === id ? { ...p, checked: !p.checked } : p)
+            );
+        })
     };
 
     return (
@@ -21,16 +42,16 @@ function ProductList() {
             {products.map(product => (
                 <div
                     key={product.id}
-                    onClick={() => toggleProduct(product.id)}
-                    className={`product-item ${product.checked ? 'checked' : ''}`}
+                    //onClick={() => toggleProduct(product.id)}
+                    className={`product-item ${product.available ? 'checked' : ''}`}
                 >
                     <input
                         type="checkbox"
-                        checked={product.checked}
+                        checked={product.available}
                         onChange={() => toggleProduct(product.id)}
                         onClick={e => e.stopPropagation()}
                     />
-                    {product.name}
+                    {product.dishName}
                 </div>
             ))}
         </div>
